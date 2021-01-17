a7edb781dc67fa9e66fc3153fffeba58dd8fc8d5
Amit Thapliyal <67275253+Thapsamit@users.noreply.github.com>
2021-01-17 21:20:42 +0530

    Update snowfall.js

--- snowfall.js
+++ snowfall.js
@@ -1,14 +1,14 @@
-function createHeart()
+function createSnow()
 {
-    let heart = document.createElement("i");
-    heart.className = "fas fa-snowflake";
-    document.body.appendChild(heart);
-    heart.style.animationDuration = Math.random()*2+5+"s";
-    heart.style.left = Math.random()*100 + "vw";
-    heart.style.zIndex = 2;
+    let snow = document.createElement("i");
+    snow.className = "fas fa-snowflake";
+    document.body.appendChild(snow);
+    snow.style.animationDuration = Math.random()*2+5+"s";
+    snow.style.left = Math.random()*100 + "vw";
+    snow.style.zIndex = 2;
      setTimeout(()=>{
-         heart.remove();
+         snow.remove();
      },5000);
      
 }
-setInterval(createHeart,100);
+setInterval(createSnow,100);
